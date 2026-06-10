912a9add58cbf3f22b23f48611bef4267a91e476
Zackary Sturtevant <zackarysturtevant@my.unt.edu>
2026-06-10 18:17:39 -0500

    Update t.cpp

---
+++ t.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // for input output
+#include <cmath> // for math
+#include<cstring>// for c-strings
+#include <string>  // for strings
+#include <cstdlib> // for random , rand function
+#include <stdio.h>  // for printf  printf(“format string”, parameters)
+#include <ctime> // for random
+#include <cassert> // for testing
+#include <iomanip>
+#include <sstream> // for stringstream
+#include <fstream> //for file reading
+
+using namespace std;
+
+int main ( )
+{
+    cout<<"hello";
+return 0;
+}
+
